5a59965a7e8fc6320994f841ddbaaa518edc8ced
Håvard Fjalstad Jensen <269408@stud.inn.no>
2023-11-25 17:21:12 +0100

    New code

--- BankSystem/Account.h
+++ BankSystem/Account.h
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 
 using namespace std;
@@ -13,11 +11,16 @@ private:
 	int accountType;
 
 public:
-	string getName() {
+	Account() {
+
+	}
+	Account* parentAccount;
+
+	string getCustomerName() {
 		return customerName;
 	}
-	string setName(string newName) {
-		return customerName = newName;
+	string setCustomerName(string newName) {
+		return this->customerName = newName;
 	}
 	int getAccNum() {
 		return accountNumber;
@@ -34,3 +37,95 @@ public:
 
 };
 
+
+class Currrent_Account : public Account {
+
+private:
+	
+	string accountName;
+	int amount;
+	Currrent_Account* subAccounts[5];
+
+public:
+	
+	int subAccountsCount = 0;
+	Currrent_Account* parentAccount;
+
+	string getAccountName() {
+		return accountName;
+	}
+	string setAccountName(string newAccountName) {
+		return accountName = newAccountName;
+	}
+	int getAmount() { return amount; }
+	int setAmount(int newAmount) { return amount += newAmount; }
+	
+
+	void Deposit(int amountToDeposit) {	
+		setAmount(amountToDeposit);
+	}
+
+	void displayBalance() {
+		cout << "\nAmount: " << this->getAmount() << " $ " ; 
+	}
+
+	Currrent_Account* createSubAccount() {
+		
+		Currrent_Account* subAccount = new Currrent_Account();
+		
+		subAccount->setAccountName("SubAccount01");
+		subAccount->setCustomerName("Peter");
+		subAccount->setAmount(0);
+		
+		subAccounts[subAccountsCount] = subAccount;
+		
+		subAccountsCount++;
+		return subAccount;
+	}
+
+	Currrent_Account* switchAccount(Currrent_Account* parentAccount) {
+
+		if (subAccountsCount >= 2) {
+			subAccounts[1]->setAccountName("SubAccount02");
+			subAccounts[2]->setAccountName("SubAccount03");
+
+		}
+	
+		
+		for (int i = 0; i < subAccountsCount; i++)
+		{
+			
+				cout << "[" << i << "] " << subAccounts[i]->getAccountName() << "\n";
+			
+		}
+		
+		int input;
+		cout << "Which account would you like to access? Input a number corresponding to the account: " << "Press 9 to go back to main account";
+		cin >> input;
+		if (input == 9) {
+			return parentAccount;
+		}
+		else
+		return subAccounts[input];
+
+
+	}
+
+	void backToMainAccount() {
+
+
+	}
+
+};
+
+class SavingAccount : public Currrent_Account {
+
+private:
+
+
+public:
+
+
+
+
+};
--- BankSystem/BankSystem.cpp
+++ BankSystem/BankSystem.cpp
@@ -1,40 +1,116 @@
 
 #include <iostream>
-#include "Account.h";
+#include <ctime>
+
+#include <chrono>
+#include "Account.h"
+#pragma warning(disable : 4996)
+
 using namespace std;
 
+const std::string currentDateTime() {
+	time_t     now = time(0);
+	struct tm  tstruct;
+	char       buf[80];
+	tstruct = *localtime(&now);
 
+	// Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+	// for more information about date/time format
+
+	strftime(buf, sizeof(buf), "%X", &tstruct);
+
+	return buf;
+}
+
+int InputGreeting(Account account) {
+
+	if (currentDateTime() > "12:00:00") cout << "Good afternoon " << account.getCustomerName();
+	else if (currentDateTime() > "18:00:00" && currentDateTime() < "04:00:00") cout << "Good evening " << account.getCustomerName();
+	else cout << "Good morning " << account.getCustomerName();
+
+	int input;
+	cout << "\nPlease select your account type: ";
+	cout << "\nPress 1 for your current account ";
+	cout << "\nPress 2 for your savings account ";
+
+	cin >> input;
+	return input;
+
+}
 int main()
 {
 	Account account;
+	account.setCustomerName("John");
 
+	Currrent_Account* currAcc = new Currrent_Account();
+	Currrent_Account* mainCurrentAccount = currAcc;
+	currAcc->setAccountName("Main Account");
+	
+	
+
+	/*int user = InputGreeting(account);*/
+	
 	int input;
 
-	while (true) {
-		cout << "\n___________________" << endl;
-		cout << "\nPress 1 to make new persons " << endl;
-		cout << "\nPress 2 to print info of persons " << endl;
-		cout << "\nPress 3 to create new person" << endl;
-		cout << "\nPress 4 to create change name of person" << endl;
-		cout << "\nPress 5 to sort by age" << endl;
-		cout << "\nPress 6 to delete a person" << endl;
+	cout << mainCurrentAccount->getAccountName();
 
+		while (true) {
+			cout << "\n___________________" << endl;
+			cout << "\nPress 1 to deposit money into your account " << endl;
+			cout << "\nPress 2 to dispaly balance of your chosen account " << endl;
+			cout << "\nPress 3 to create new account within your account" << endl;
+			cout << "\nPress 4 to switch to new sub account" << endl;
+			cout << "\nPress 5 to switch to back to main account" << endl;
 
-		cin >> input;
+			cin >> input;
 
-		switch (input) {
+			if (input == 0)break;
 
-		case 1: {
+			switch (input) {
 
-			break;
+			case 1: {
+
+				int amountToDeposit;
+				cout << "How much would you like to deposit? ";
+				cin >> amountToDeposit;
+				currAcc->Deposit(amountToDeposit);
+
+				break;
+			}
+			case 2: {
+				currAcc->displayBalance();
+
+				break;
+			}
+			
+			case 3: {
+				currAcc->createSubAccount();
+				break;
+			}
+			case 4: {
+				
+				currAcc = currAcc->switchAccount(mainCurrentAccount);
+				cout << "Current account: " << currAcc->getAccountName();
+				break;
+
+			}
+			case 5: {
+				if (currAcc == mainCurrentAccount) cout << "\nYou are already in main account";
+				else currAcc = mainCurrentAccount;
+				break;
+			}
+					
+			default:
+				cout << "Unknown input";
+				
+			};
+
 		}
+	
+	
+	
 
-		default:
-			cout << "Unknown input";
 
-		};
-
-	}
 }
 
 
